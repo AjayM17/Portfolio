c21134d69c22d015c1c4b08f29d0f222df268690
AjayM17 <ajaymandrawal17@gmail.com>
2020-03-22 08:28:26 +0530

    new changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component'
+import { ProjectsComponent } from './projects/projects.component'
 
 const routes: Routes = [{
   path:'',
@@ -10,6 +11,10 @@ const routes: Routes = [{
   {
     path:'home',
     component:HomeComponent
+  },
+  {
+    path:'projects',
+    component:ProjectsComponent
   }
 
 ];
